3cc44156ac7e1cfd4c24aefc3c75739c4c521e14
Mukund Vinayak <mukund.vinayak3@gmail.com>
2023-04-20 21:32:16 +0530

    check palindrome added

---
+++ src/main/java/com/JavaStreams/StringsNumbersMaths/PalindromeString.java
@@ -0,0 +1,40 @@
+package com.JavaStreams.StringsNumbersMaths;
+
+import java.util.stream.IntStream;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Check whether a string is palindrome or not
+     * approach 1. use meet in the middle approach using a. while loop
+     * b. for-loop
+     * approach 2. using stringbuilder
+     * approach 3. using stream
+     * */
+    private static boolean checkPalindrome(String str){
+        int left = 0;
+        int right = str.length()-1;
+        while (left < right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private static boolean checkPalindromeStringbuilder(String str){
+        if(str.equals(new StringBuilder(str).reverse().toString()))
+            return true;
+        return false;
+    }
+
+    private static boolean checkPalindromeStream(String str){
+        IntStream.range(0, str.length()/2)
+                .noneMatch(i-> str.charAt(i) != str.charAt(str.length()-1-i));
+    }
+}
